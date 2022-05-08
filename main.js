7d475cae46328cbba0d0a81633e7543918d20679
gioiascari <gioia.ascari96@gmail.com>
2022-05-08 22:09:01 +0200

    da completare

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("Ok Js:)");
 // Utilities
 // Playing section
-let nowPlaying = document.querySelector(".nowPlaying");
+let nowPlaying = document.querySelector(".now-playing");
 let trackArt = document.querySelector(".track-art");
 let trackName = document.querySelector(".track-name");
 let trackAuthor = document.querySelector(".track-author");
@@ -14,8 +14,9 @@ let randomBtn = document.querySelector(".random-track");
 // Volume up/down, current time section
 let seekSlider = document.querySelector(".seek-slider");
 let volumeSlider = document.querySelector(".volume-slider");
-let currentTime = document.querySelector(".current-time");
+let current_Time = document.querySelector(".current-time");
 let totalDuration = document.querySelector(".total-duration");
+let randomIcon = document.querySelector(".fa-random");
 let waves = document.getElementById("waves");
 let currentTrack = document.createElement("audio");
 
@@ -24,10 +25,28 @@ let currentTrack = document.createElement("audio");
 let trackI = 0;
 let isPlaying = false;
 let isRandom = false;
-let timer;
+let timer_loader;
 
 //Music List
 const musicList = [
+  {
+    author: "Alan Walker",
+    song: "Faded",
+    audio: "audio/music_Faded.mp3",
+    image: "img/faded.png",
+  },
+  {
+    author: "Alan Walker",
+    song: "Faded",
+    audio: "audio/music_Faded.mp3",
+    image: "img/faded.png",
+  },
+  {
+    author: "Alan Walker",
+    song: "Faded",
+    audio: "audio/music_Faded.mp3",
+    image: "img/faded.png",
+  },
   {
     author: "Alan Walker",
     song: "Faded",
@@ -39,7 +58,7 @@ const musicList = [
 loadTrack(trackI);
 
 function loadTrack(trackI) {
-  clearInterval(timer);
+  clearInterval(timer_loader);
   reset();
   //   Inizializzo i componenti per inserirci i dati
   currentTrack.src = musicList[trackI].audio;
@@ -51,7 +70,7 @@ function loadTrack(trackI) {
   // Indice che mi riferisce quale canzone sto ascoltando sulla parte superiore del player
   nowPlaying.textContent =
     "Playing music" + (trackI + 1) + "Of" + musicList.length;
-  timer = setInterval(setUpdate, 1000);
+  timer_loader = setInterval(setUpdate, 1000);
   currentTrack.addEventListener("ended" + nextBtn);
 
   randomColor(); //Funzione sfondo randomico
@@ -64,8 +83,8 @@ function randomColor() {
   let hex;
   function rColor(hex) {
     for (let i = 0; i < 6; i++) {
-      let x = Math.round(Math.random() * hexValue.length);
-      let y = hex[x];
+      let x = Math.round(Math.random() * 14);
+      let y = hexValue[x];
       hex += y;
     }
     return hex;
@@ -79,13 +98,13 @@ function randomColor() {
   document.body.style.background = gradientColor;
 }
 function reset() {
-  currentTime.textContent = "00:00";
+  current_Time.textContent = "00:00";
   totalDuration.textContent = "00:00";
   seekSlider.value = 0;
 }
 // Function random track
 function randomTrack() {
-  isPlaying ? pauseRandom() : playRandom();
+  isRandom ? pauseRandom() : playRandom();
 }
 function playRandom() {
   isRandom = true;
@@ -111,4 +130,71 @@ function playTrack() {
   trackArt.classList.add("rotate");
   waves.classList.add("loader");
   playBtn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+function pauseTrack() {
+  currentTrack.pause();
+  isPlaying = false;
+  trackArt.classList.remove("rotate");
+  waves.classList.remove("loader");
+  playBtn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+function nextTrack() {
+  if (trackI < musicList.length - 1 && isRandom === false) {
+    trackI += 1;
+  } else if (trackI < musicList.length - 1 && isRandom === true) {
+    let randomIndex = Number.parseInt(Math.random() * musicList.length);
+    trackI = randomIndex;
+  } else {
+    trackI = 0;
+  }
+  loadTrack(trackI);
+  playTrack();
+}
+function prevTrack() {
+  if (trackI > 0) {
+    trackI -= 1;
+  } else {
+    trackI = musicList.length - 1;
+  }
+  loadTrack(trackI);
+  playTrack();
+}
+function seekTo() {
+  let seekto = currentTrack.duration * (seekSlider.value / 100);
+  currentTrack.currentTime = seekto;
+}
+function setVolume() {
+  currentTrack.volume = volumeSlider.value / 100;
+}
+function setUpdate() {
+  let seekPosition = 0;
+  if (!isNaN(currentTrack.duration)) {
+    seekPosition = currentTrack.currentTime * (100 / currentTrack.duration);
+    seekSlider.value = seekPosition;
+
+    let currentMinutes = Math.floor(currentTrack.currentTime / 60);
+    let currentSeconds = Math.floor(
+      currentTrack.currentTime - currentMinutes * 60
+    );
+    let durationMinutes = Math.floor(currentTrack.duration / 60);
+    let durationSeconds = Math.floor(
+      currentTrack.duration - durationMinutes * 60
+    );
+
+    if (currentSeconds < 10) {
+      currentSeconds = "0" + currentSeconds;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = "0" + durationSeconds;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = "0" + durationMinutes;
+    }
+
+    current_Time.textContent = currentMinutes + ":" + currentSeconds;
+    totalDuration.textContent = durationMinutes + ":" + durationMinutes;
+  }
 }
