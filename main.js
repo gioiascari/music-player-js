ddd1e5cc37ae67206462ceb2a599de40b0a482de
gioiascari <gioia.ascari96@gmail.com>
2022-05-08 18:34:56 +0200

    init js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Ok Js:)");
+// Utilities
+let nowPlaying = document.querySelector(".nowPlaying");
+let trackArt = document.querySelector(".track-art");
+let trackName = document.querySelector(".track-name");
+let trackAuthor = document.querySelector(".track-author");
+
+// /Utilities
