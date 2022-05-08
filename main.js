1b356891079b83b047279c9bd51a6711d9291e51
gioiascari <gioia.ascari96@gmail.com>
2022-05-08 18:39:05 +0200

    inizializzati btn in js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 console.log("Ok Js:)");
 // Utilities
+// Playing section
 let nowPlaying = document.querySelector(".nowPlaying");
 let trackArt = document.querySelector(".track-art");
 let trackName = document.querySelector(".track-name");
 let trackAuthor = document.querySelector(".track-author");
-
+// Buttons section
+let playBtn = document.querySelector(".playPause-track");
+let nextBtn = document.querySelector(".next-track");
+let prevBtn = document.querySelector(".prev-track");
+let repeatBtn = document.querySelector(".repeat-track");
+let randomBtn = document.querySelector(".random-track");
 // /Utilities
